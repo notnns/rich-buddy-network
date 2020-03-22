f09953060d3c3949d30940b106e359197184cbc8
unknown <nns.noreply@gmail.com>
2020-03-23 01:33:29 +0600

    first

---
+++ src/fakeData/buddy.js
@@ -0,0 +1,186 @@
+var buddies =
+    [
+        {
+            "id": "1",
+            "name": "Mark Zuckerberg",
+            "img": "https://i.imgur.com/4LBCVm9.jpg",
+            "company": "Mukh Boi, Kan pete shune roi",
+            "salary": 100,
+            "email": "joker.barg@fakemail.com",
+            "phone": "+0248865064"
+
+        },
+
+        {
+            "id": "2",
+            "name": "Jeff Bezos",
+            "img": "https://i.imgur.com/ji7lcfR.jpg", 
+            "company": "Number 1 Online Market",
+            "salary": 200,
+            "email": "ema.john@fakemail.com",
+            "phone": "+2069305629"
+
+        },
+
+
+        {
+            "id": "3",
+            "name": "Bill Gates",
+            "img": "https://i.imgur.com/3FBZqB5.jpg", 
+            "company": "Janalar Karigor",
+            "salary": 300,
+            "email": "billu.vai@fakemail.com",
+            "phone": "+4521746719"
+
+        },
+
+
+        {
+            "id": "4",
+            "name": "Warren Buffett",
+            "img": "https://i.imgur.com/oy83HEt.jpg", 
+            "company": "Bura Manush Ami",
+            "salary": 400,
+            "email": "buffet.khabo@fakemail.com",
+            "phone": "+6619977706"
+
+        },
+
+        {
+            "id": "5",
+            "name": "Mukesh Ambani",
+            "img": "https://i.imgur.com/OtSGpaM.jpg", 
+            "company": "Real King of India",
+            "salary": 500,
+            "email": "zio.4g@fakemail.com",
+            "phone": "+7209979545"
+
+        },
+
+
+        {
+            "id": "6",
+            "name": "Jack Ma",
+            "img": "https://i.imgur.com/6ECm7II.jpg", 
+            "company": "Number 1 Marketplace",
+            "salary": 600,
+            "email": "ali.baba@fakemail.com",
+            "phone": "+2266557955"
+
+        },
+
+
+        {
+            "id": "7",
+            "name": "Donald Trump",
+            "img": "https://i.imgur.com/OUYY05R.jpg", 
+            "company": "Facebook VIP Profile",
+            "salary": 700,
+            "email": "khepa.pagla@fakemail.com",
+            "phone": "+5650275765"
+
+        },
+
+
+        {
+            "id": "8",
+            "name": "Larry Page",
+            "img": "https://i.imgur.com/yBLY4tP.jpg", 
+            "company": "Google a search dao",
+            "salary": 800,
+            "email": "google.boss@fakemail.com",
+            "phone": "+8176607257"
+
+        },
+
+
+        {
+            "id": "9",
+            "name": "Michael Bloomberg",
+            "img": "https://i.imgur.com/U9TspRP.jpg", 
+            "company": "TV Channel sob amar",
+            "salary": 900,
+            "email": "keda.tui@fakemail.com",
+            "phone": "+5942405660"
+
+        },
+
+
+        {
+            "id": "10",
+            "name": "Shakib Khan",
+            "img": "https://i.imgur.com/50fOuwZ.jpg", 
+            "company": "World Class Cinema Coaching Center",
+            "salary": 1000,
+            "email": "number.one@fakemail.com",
+            "phone": "+7367929572"
+
+        },
+
+
+        {
+            "id": "11",
+            "name": "Sakib Al Hasan",
+            "img": "https://i.imgur.com/GYXH3bp.jpg", 
+            "company": "Khelar Maath",
+            "salary": 1100,
+            "email": "bangladeher.jan@fakemail.com",
+            "phone": "+2122699626"
+
+        },
+
+
+        {
+            "id": "12",
+            "name": "Mashrafi Bin Mortuza",
+            "img": "https://i.imgur.com/4WPOkt1.png", 
+            "company": "Desh er Jonno",
+            "salary": 1200,
+            "email": "guru.mash@fakemail.com",
+            "phone": "+4475659714"
+
+        },
+
+
+        {
+            "id": "13",
+            "name": "Sefat Ullah",
+            "img": "duhttps://i.imgur.com/1WNXQlM.jpgmmy", 
+            "company": "Manush Hobi Foundation",
+            "salary": 1300,
+            "email": "sefuda@fakemail.com",
+            "phone": "+8884607787"
+
+        },
+
+
+        {
+            "id": "14",
+            "name": "Shamim Osman",
+            "img": "https://i.imgur.com/9wVL1Vl.jpg", 
+            "company": "Khela Hobe",
+            "salary": 1400,
+            "email": "khela.hobe@fakemail.com",
+            "phone": "+512 713 6140"
+
+        },
+
+
+        {
+            "id": "15",
+            "name": "the NNS",
+            "img": "https://i.imgur.com/pmz8tZo.jpg", 
+            "company": "THE NNS",
+            "salary": 1500,
+            "email": "the.nns@fakemail.com",
+            "phone": "+321 264 3525"
+
+        },
+
+        
+
+
+    
+    ];
+
+export default buddies;
